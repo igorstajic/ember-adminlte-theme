540eb6f302d77844f6d8a9ceb271b628e6529d90
igor <igorstajic273@gmail.com>
2017-05-29 19:01:27 +0200

    Removed active link management.

--- addon/components/main-sidebar.js
+++ addon/components/main-sidebar.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { computed, isEmpty, $, Component } = Ember;
+const { isEmpty, $, Component } = Ember;
 
 export default Component.extend({
   tagName: 'aside',
@@ -8,18 +8,6 @@ export default Component.extend({
   sidebarHeight: function(){
     return this.$(".sidebar").height();
   },
-  
-  routeName: computed("appRoute.controller.currentRouteName", function(){
-    return this.get('appRoute.controller.currentRouteName')
-  }),
-
-  routeUrl: computed("appRoute.router.url", function(){
-    return this.get("appRoute.router.url").replace(/\?.*/, "")
-  }),
-
-  urlChanged: function(){
-    this.initSidebarByRoute()
-  }.observes("appRoute.router.url"),
 
   didInsertElement: function(){
     var that = this;
@@ -27,61 +15,8 @@ export default Component.extend({
     Ember.run.next(function(){
       that.treeMenu();
     });
-
-    Ember.run.next( () => {
-      this.initSidebarByRoute();
-    })
   },
 
-  initSidebarByRoute: function(){
-    let that = this;
-    let $navItems = this.$("li", ".sidebar");
-    $navItems.removeClass("active");
-    let currentUrl = that.get("routeUrl");
-    let element = this.getActiveElement(currentUrl)
-    if(element) this.activeCurrent(element)
-  }.observes("routeUrl"),
-
-  getActiveElement: function(currentUrl){
-    let links = this.$("a", ".sidebar"),
-      el = null;
-
-    let elems = links.filter((i, elem) => {
-      let url = ($(elem).attr("href") || "").replace("#", "");
-      if(isEmpty(url)) return false;
-      if(url == currentUrl) el = elem
-      return ~currentUrl.indexOf(url)
-    }).sort(function(a, b){ 
-      if(a == b) return 0
-      return a < b ? 1 : -1
-    })
-    return (el ? el : elems[0])
-  },
-
-  initRouter(){
-    let _name = this.get("appRoute.controller.currentRouteName");
-    let _url = this.get("appRoute.router.url");
-    this.set("routeName", _name);
-    this.set("routeUrl", _url);
-  },
-
-  activeCurrent(element) {
-
-    let that = this , navs = [];
-    let isRoot = function(ele) { 
-      return that.$(ele).parent().hasClass("sidebar-menu");
-    };    
-    while ( !isRoot(element) ){
-      let father = that.$(element).parent();
-      navs.push(father);
-      element = father;
-    }
-    navs.push(element);
-    $.each( navs ,(index, nav) => {
-      nav.addClass("active");
-    })
-
-  },
 
   treeMenu(){
     this.$(".sidebar li a").click(function(e){
